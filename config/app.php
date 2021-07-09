9e32dcc1b265464d9f5d673563319eccb2e730bb
Mathieu TUDISCO <mathieu@tudisco.fr>
2021-07-09 02:24:44 +0200

    Fix IdeHelper provider in config.

--- config/app.php
+++ config/app.php
@@ -167,7 +167,6 @@ return [
         Crater\Providers\EventServiceProvider::class,
         Crater\Providers\RouteServiceProvider::class,
         Crater\Providers\DropboxServiceProvider::class,
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
     ],
 
     /*
